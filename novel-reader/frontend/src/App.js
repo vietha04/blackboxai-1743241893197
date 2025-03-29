fe607affbd174860e2998a322f3c564a1a7d00ff
vietha04 <153857999+vietha04@users.noreply.github.com>
2025-03-29 18:10:27 +0700

    Add novel-reader/frontend/src/App.js 1743246627152

--- novel-reader/frontend/src/App.js
+++ novel-reader/frontend/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Reader from './components/Reader';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/novel/:id/chapter/:chapterIndex" component={Reader} />
-          <Route path="/" exact>
-            <h1>Novel Reader Home</h1>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Novel Reader Home</h1>
+    </div>
   );
 }
 
